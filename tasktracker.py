abc703ccb4e1a88e96117cd208edc45f25a7f89b
William Diefenbach <bdiefenbach@roadrunner.com>
2021-11-04 15:55:40 -0400

    changed the tasktracker format so that it does not access an outside file rather makes a list using an array

--- tasktracker.py
+++ tasktracker.py
@@ -1,11 +1,13 @@
 import stdio
+import sys
+import os
 
 # Building a task tracking application using python
-# The path function makes sure that there is a text file created to store tasks
 
-from pathlib import Path
-Path("/home/diefenbw/tasktracker2/tasktracker.md").touch()
+values = [(False, "Eat Breakfast")
+         ,()]
 
-stdio.writeln("Enter tasks that you have to do. If you are unsure about the task
-        end that task with a '?'")
+current = 0
 
+while True:
+    os.system("clear")
